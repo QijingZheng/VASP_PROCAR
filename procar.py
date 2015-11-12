69b9f06b5d98189557ff26e63daf3a5f6c48b19a
QijingZheng <zqj.kaka@gmail.com>
2015-11-12 21:38:04 +0800

    Update procar.py

--- procar.py
+++ procar.py
@@ -12,7 +12,9 @@ def findfloats(line):
     1.0 1000 -1.0 +1 +1.0
     """
     # print line
-    floatpat =  r"\b[-+]?\d+\.\d+\b|\b[-+]?\d+\b"
+    # floatpat =  r"\b[-+]?\d+\.\d+\b|\b[-+]?\d+\b"
+    floatpat =  r"[-+]?\d+.\d+|[-+]?\d+"
+    
     return re.findall(floatpat, line)
 
 def Gaussian(x, x0, sigma=0.1):
